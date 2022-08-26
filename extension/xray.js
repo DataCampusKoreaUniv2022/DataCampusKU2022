b20f8f0f360ac5080ea347f2da2e92eb897b66c6
hanjoo0211 <hanjoo0211@gmail.com>
2022-08-26 13:35:10 +0900

    chore: remove dummy files

--- extension/xray.js
+++
@@ -1,9 +0,0 @@
-chrome.storage.local.get(['image', 'width', 'height'], function(result) {
-    let image = document.createElement('img');
-    image.src = result.image;
-    image.style.width = result.width;
-    image.style.height = result.height;
-
-    let body = document.querySelector('body');
-    body.append(image);
-});
